cb7f2b275036b92386093257b168134262343233
nordras <igorkmail@gmail.com>
2019-10-14 17:54:08 -0300

    Conectando mock api

--- components/Header.js
+++ components/Header.js
@@ -4,15 +4,21 @@ const linkStyle = {
   marginRight: 15
 };
 
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+};
+
 const Header = () => (
-  <div>
+  <section style={layoutStyle}>
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
     <Link href="/about">
       <a style={linkStyle}>About</a>
     </Link>
-  </div>
+  </section>
 );
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import Link from 'next/link'
 import Header from '../components/Header'
+import axios from 'axios'
 
 const index = (props) => {
   
   return (
     <>
       <Header/>
-      <h1>Hello next 2 {props.nome}</h1>
+      <h1>Hello next 2</h1>
       <Link href="/about">
         <a>About Page</a>
       </Link>
+      <h4>Dados do mockApi</h4>
+      <section>
+        {/* {JSON.stringify(props.data)}} */}
+        {
+          props.data.map(item => {
+            return (
+            <div key={item.id}>
+              <h2>{item.name}</h2>
+              <img src={item.avatar}/>
+            </div>)
+          })
+        }
+      </section>
     </>
   )
 }
 
-index.getInitialProps = () => {
+index.getInitialProps = async () => {
   console.log('Logando pagina index')
+  const api = 'https://5da4daf857f48b0014fba3e4.mockapi.io/v1'
+  const data = await axios.get(`${api}/user`)
   return {
-    nome: 'Igor'
+    data: data.data
   }
 }
 
